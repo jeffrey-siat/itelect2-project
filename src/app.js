302e3115e0c66c2bee469d18bea94993af221971
Jeffrey Jr Siat <jeffrey_jr_siat@dlsl.edu.ph>
2026-07-22 21:47:40 +0800

    GT4: Add async functions to app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
-import { formatDate, validateTask, mergeTaskUpdate } from './utils.js';
+import { formatDate, validateTask, mergeTaskUpdate, createTask } from './utils.js';
+import { fetchSampleUsers } from './api.js';
 
 console.log(formatDate(new Date("2026-07-22")));
 console.log(validateTask());
 console.log(validateTask({ title: "Finish thesis", dueDate: "2026-08-01" }));
 console.log(mergeTaskUpdate({ title: "Old" }, { title: "New" }));
+
+const runGT4 = async () => {
+  try {
+    const users = await fetchSampleUsers();
+    console.log(users);
+
+    const task = createTask({ title: "Finish thesis draft", dueDate: "2026-08-01" });
+    console.log(task);
+  } catch (err) {
+    console.error("Something went wrong:", err.message);
+  }
+};
+
+runGT4();
