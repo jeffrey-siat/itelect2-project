644dc0ac8baca34ede8c1fa80ae60fa397728464
Jeffrey Jr Siat <jeffrey_jr_siat@dlsl.edu.ph>
2026-09-11 11:48:51 +0800

    Add title validation to Task model

--- models/task.cjs
+++ models/task.cjs
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Task extends Model {
     static associate(models) {
@@ -9,7 +7,11 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Task.init({
-    title: DataTypes.STRING,
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { notEmpty: { msg: 'title is required' } }
+    },
     dueDate: DataTypes.DATE,
     completed: DataTypes.BOOLEAN,
     userId: DataTypes.INTEGER
